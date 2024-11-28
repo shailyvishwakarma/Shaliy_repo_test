74931ead136bfc16b7eb85106d2f0cf96ff3b876
shailyvishwakarma <shaily.vishwakarma@pabbly.com>
2024-11-28 15:17:39 +0530

    JS

---
+++ app.js
@@ -0,0 +1,11 @@
+// Simple JavaScript code to add two numbers
+
+// Declare two numbers
+let num1 = 5;
+let num2 = 3;
+
+// Add the numbers
+let sum = num1 + num2;
+
+// Output the result
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
